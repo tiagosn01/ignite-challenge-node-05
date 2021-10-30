36aea0ff7e10ef44c208e6425083bb94d50ce459
Tiago Silva <tiagosilva0922@gmail.com>
2021-10-30 12:48:18 -0300

    fix: queries resolved, users and games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,30 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    const games = await this.repository
+      .createQueryBuilder("game")
+      .where("game.title ILIKE :title", {title: `%${param}%`})
+      .getMany()
+
+      return games
+    
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+    SELECT COUNT(id)
+    FROM games
+  `); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const users = await this.repository
+    .createQueryBuilder()
+    .relation(Game, "users")
+    .of(id)
+    .loadMany()
+
+    return users
   }
 }
